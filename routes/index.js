2070861732ceda15894822e978388712b71f5700
Tom Pierce <tompierce4@gmail.com>
2023-11-23 14:23:05 +0000

    messages are read from and saved to mondodb

--- routes/index.js
+++ routes/index.js
@@ -1,34 +1,28 @@
 const express = require("express");
 const router = express.Router();
+const mongoose = require("mongoose");
+const Message = require("../models/message");
+require("dotenv").config();
+mongoose
+  .connect(process.env.MESSAGES_DB)
+  .then((x) =>
+    console.log(`Connected the Database: "${x.connections[0].name}"`),
+  )
+  .catch((err) => console.log("Error connecting to mongo", err));
 
-const messages = [
-  {
-    text: "Hi there!",
-    user: "Amando",
-    added: new Date(),
-  },
-  {
-    text: "Hello World!",
-    user: "Charles",
-    added: new Date(),
-  },
-];
-
-/* GET home page. */
-router.get("/", function (req, res, next) {
-  res.render("index", { title: "Mini Message Board", messages: messages });
+router.get("/", async function (req, res, next) {
+  await Message.find().then((messages) => {
+    res.render("index", { title: "Mini Message Board", messages: messages });
+  });
 });
 
-router.get("/new", function (req, res, next) {
-  res.render("form");
-});
-
-router.post("/new", function (req, res, next) {
-  messages.push({
+router.post("/new", async function (req, res, next) {
+  const message = new Message({
     text: req.body.message,
     user: req.body.user,
     added: new Date(),
   });
+  await message.save();
   res.redirect("/");
 });
 
